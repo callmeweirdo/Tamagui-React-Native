70b874e7586eedef1f11281453355135365dc991
callmeweirdo <ogbandav102@gmail.com>
2024-07-19 04:31:12 +0100

    ive initialized this app after trying it out lots of times on the github codespaces i fininally had to set it up on my Ubuntu Dev environment

---
+++ app/(drawer)/(home)/_layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Drawer from "expo-router/drawer";
+
+const Layout = () => {
+  return (
+      <>
+          <Drawer>
+              
+          </Drawer>
+      </>
+  )
+}
+
+export default _layout;
