eb2c10fe5e14839968fe32f3b72c071cd498b27c
callmeweirdo <ogbandav102@gmail.com>
2024-07-23 16:21:42 +0100

    added some styling to the index contents

--- app/(drawer)/home/index.tsx
+++ app/(drawer)/home/index.tsx
@@ -1,11 +1,12 @@
 // app/(drawer)/home/index.tsx
 import { Link } from 'expo-router';
 import React from 'react';
-import { View, Text, Card, Main } from 'tamagui';
-import { Subtitle, Title } from '~/tamagui.config';
+import { View, Text, Card, Main, Input } from 'tamagui';
+import { Container, Subtitle, Title } from '~/tamagui.config';
 import { useQuery } from '@tanstack/react-query';
 import { getTrending } from '~/services/api';
 import { ImageBackground } from 'react-native';
+import { YStack } from 'tamagui';
 
 export default function HomeScreen() {
 
@@ -19,7 +20,15 @@ export default function HomeScreen() {
       <ImageBackground
         source={{ uri: `https://image.tmdb.org/t/p/w1920_and_h600_multi_faces_filter(duotone,032541,01b4e4)/ghQrKrcEpAlkzBuNoOCSxHQXWqw.jpg` }}
         style={{ width: '100%', height: 200 }}
-      />
+      >
+
+        <Container>
+          <YStack>
+            <Title style={{ color: '#fff' }} enterStyle={{ opacity: 0, scale: 1.5, y: -10 }} animation="bouncy">Trending</Title>
+            <Input />
+          </YStack>
+        </Container>
+      </ImageBackground>
     </Main>
   );
 }
