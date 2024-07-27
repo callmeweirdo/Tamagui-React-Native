39b0745ba3d7425f7933fefaa573f64b190c9fb3
callmeweirdo <ogbandav102@gmail.com>
2024-07-27 18:09:31 +0100

    finaly completed the project and now am just gonna optimise the code and try to publish it

--- app/(drawer)/favorites/index.tsx
+++ app/(drawer)/favorites/index.tsx
@@ -10,6 +10,7 @@ import useDebounce from '~/utils/useDebounce';
 import MovieCard from '~/components/MovieCard';
 import { useMMKVObject } from 'react-native-mmkv';
 import { Favorite } from '~/interfaces/moviesinterfaces';
+import Animated from 'react-native-reanimated';
 
 export default function HomeScreen() {
   const [favorites, setFavorites] = useMMKVObject<Favorite[]>('favorites');
@@ -25,16 +26,16 @@ export default function HomeScreen() {
             favorites?.map((fav) => (
               <Link key={fav.id} href={`/(drawer)/favorites/${fav.mediaType}/${fav.id}`} asChild>
                 <ListItem
-                  p={20}
+                  p={10}
                   theme={'alt1'}
                   title={fav.name}
                   size={'$3'}
                   icon={() => (
-                    <Image
+                    <Animated.Image
                       source={{ uri: `https://image.tmdb.org/t/p/w500${fav.thumb}` }}
                       style={{ width: 150, height: 150, borderRadius: '10px' }}
-
-                    ></Image>
+                    
+                    ></Animated.Image>
                   )}>
 
                 </ListItem>
